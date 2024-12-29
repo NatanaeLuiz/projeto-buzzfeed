701bdfc1e1d81aa37ebb3e9f689b9af1ef028001
Natanael Rolim <natanael.10@hotmail.com>
2024-12-29 18:58:52 -0300

    style quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
-
+  finished: boolean = false;
 }
